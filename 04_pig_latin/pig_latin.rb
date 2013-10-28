8eaa0ef71c9b88cc14a4ab88a11ff271e9ae2c28
shan <corallishan@gmail.com>
2013-10-28 19:32:41 -0400

    hard part done half

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -2,24 +2,34 @@ def translate(word)
   vowels = ["a", "e", "i", "o", "u"]
 
 
-  if vowels.include?(word[0])
+  # if vowels.include?(word[0])
     
-    word = word<<"ay"
+  #   word = word<<"ay"
 
-  else
-    new_array=[]
+  # else
+    temp=[]
+    c = "ay"
+    
     letter_array = word.split("")
-    letter_array do |n|
-    if !vowels.include?(n)
-      #remove this letter from the word to array
+    
+    first = letter_array[0]
+    begin
+    
+      unless vowels.include?(first)
+        temp.push(first)
+        letter_array.shift
+       first = letter_array[0]
+     end
 
-
-      word = word<<n
-      word = word<<"ay"
-    else
-      break
-    end
+    end while !vowels.include?(first)
+    
+    a = temp.join("")
+    b = letter_array.join("")
+    together(a,b,c)
   end
-end
+
+def together(a,b,c)
+ b+a+c 
 end
 
+translate("apple")

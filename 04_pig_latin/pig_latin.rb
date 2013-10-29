85e5f0eab7822dc7adb8b2e0ae976d4c0c10ad2c
shan <corallishan@gmail.com>
2013-10-29 12:24:15 -0400

    little change of pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -6,47 +6,28 @@ def translate(word)
       new_word_array.push(single_word(x))
     end
     final_string = new_word_array.join(" ")
-    
-
-
-
-    #   if index > 0
-    #     new_word += " "
-    #   end
-    #    new_word += single_word(w)
-    #   end
-    #   new_word
-    # end
-  else single_word(word)
+    else single_word(word)
   end
 end
 
-
-
 def single_word(word)
   vowels = ["a", "e", "i", "o"]
-
-
-    temp=[]
-    c = "ay"
-    
+  temp=[]
+  c = "ay"
     letter_array = word.split("")
-    
     first = letter_array[0]
     begin
-    
       unless vowels.include?(first)
         temp.push(first)
         letter_array.shift
        first = letter_array[0]
       end
-
     end while !vowels.include?(first)
     
-    a = temp.join("") #not vowels
-    b = letter_array.join("")
-    together(a,b,c)
-  end
+      a = temp.join("") #not vowels
+      b = letter_array.join("")
+      together(a,b,c)
+end
 
 def together(a,b,c)
  b+a+c 
@@ -56,4 +37,3 @@ end
 
 
 
- puts translate("apple cherry")
